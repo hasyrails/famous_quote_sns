4f8006e974c9bba71f3929ed789da85f5015c462
hasyrails <hasyrails@gmail.com>
2022-08-22 02:43:37 +0900

    Twitterカードによる共有設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,14 @@ module ApplicationHelper
     twitter_card = {}
     if post.present?
       if post.id.present?
-        twitter_card[:url] = "https://[herokuアプリ名].herokuapp.com/posts/#{post.id}"
-        twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/[S3バケット名]/images/#{post.id}.png"
+        twitter_card[:url] = "https://serifoo.herokuapp.com/posts/#{post.id}"
+        twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/kakugen/images/#{post.id}.png"
       else
-        twitter_card[:url] = 'https://[herokuアプリ名].herokuapp.com/'
+        twitter_card[:url] = 'https://serifoo.herokuapp.com/'
         twitter_card[:image] = "https://techpit-market-prod.s3.amazonaws.com/uploads/part_attachment/file/3883/2ce8c3fa-117a-46e2-b308-f7fed69f56c9.png"
       end
     else
-      twitter_card[:url] = 'https://[herokuアプリ名].herokuapp.com/'
+      twitter_card[:url] = 'https://serifoo.herokuapp.com/'
       twitter_card[:image] = "https://techpit-market-prod.s3.amazonaws.com/uploads/part_attachment/file/5797/9942f27f-bb05-4a0e-bd58-e9d6d462dcdd.png"
     end
     twitter_card[:title] = "タイトル"
